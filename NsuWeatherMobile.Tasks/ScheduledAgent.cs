03cf80f60d742c7631ce4ac620febf3722556590
Alexandr Tsvetkov <aleksandr.s.tsvetkov@gmail.com>
2014-07-23 01:01:26 +0700

    Initialize project

---
+++ NsuWeatherMobile.Tasks/ScheduledAgent.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using Microsoft.Phone.Scheduler;
+using Microsoft.Phone.Shell;
+
+namespace NsuWeatherMobile.Tasks
+{
+    public class ScheduledAgent : ScheduledTaskAgent
+    {
+        private int index;
+
+        /// <remarks>
+        /// ScheduledAgent constructor, initializes the UnhandledException handler
+        /// </remarks>
+        static ScheduledAgent()
+        {
+            // Subscribe to the managed exception handler
+            Deployment.Current.Dispatcher.BeginInvoke(delegate
+            {
+                Application.Current.UnhandledException += UnhandledException;
+            });
+        }
+
+        /// Code to execute on Unhandled Exceptions
+        private static void UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (Debugger.IsAttached)
+            {
+                // An unhandled exception has occurred; break into the debugger
+                Debugger.Break();
+            }
+        }
+
+        /// <summary>
+        /// Agent that runs a scheduled task
+        /// </summary>
+        /// <param name="task">
+        /// The invoked task
+        /// </param>
+        /// <remarks>
+        /// This method is called when a periodic or resource intensive task is invoked
+        /// </remarks>
+        protected override void OnInvoke(ScheduledTask task)
+        {
+            try
+            {
+                UpdateMainTile();
+                ScheduledActionService.LaunchForTest(task.Name, TimeSpan.FromMinutes(10));
+            }
+            catch (Exception)
+            {
+                ScheduledActionService.LaunchForTest(task.Name, TimeSpan.FromSeconds(60));
+            }
+
+            NotifyComplete();
+        }
+
+        public void UpdateMainTile()
+        {
+            var mainTile = ShellTile.ActiveTiles.FirstOrDefault();
+
+            if (null != mainTile)
+            {
+                FlipTileData tileData = new FlipTileData()
+                                            {
+                                                BackContent =
+                                                    string.Format("Температура сейчас:{0}",
+                                                                  NsuWeatherMobile.Common.DataLoader.LoadTemperature
+                                                                      ().Result)
+                                            };
+
+                mainTile.Update(tileData);
+            }
+        }
+    }
+}
---
+++ NsuWeatherMobile/ViewModel/TemperatureModel.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace NsuWeatherMobile.ViewModel
+{
+    public class TemperatureModel : INotifyPropertyChanged 
+    {
+        private float temperature;
+        private bool isLoaded;
+        private UpdateTemperatureCommand updateTemperatureCommand;
+        private DateTime updateTime;
+        private bool isError;
+
+        public async void UpdateTemperature()
+        {
+            IsError = false;
+            IsLoad = false;
+            try
+            {
+                Temperature = await Common.DataLoader.LoadTemperature();
+                UpdateTime = DateTime.Now;
+                IsLoad = true;
+            }
+            catch (Exception)
+            {
+                IsError = true;
+                IsLoad = false;
+            }
+
+        }
+
+        public float Temperature
+        {
+            get { return temperature; }
+            set
+            {
+                temperature = value;
+                OnPropertyChanged("TemperatureAtNsu");
+            }
+        }
+
+        public DateTime UpdateTime
+        {
+            get { return updateTime; }
+            set
+            {
+                updateTime = value;
+                OnPropertyChanged("RefreshTimeString");
+            }
+        }
+
+        public string RefreshTimeString
+        {
+            get { return string.Format("Обновлено в {0}", updateTime.ToShortTimeString()); }
+        }
+
+        public string TemperatureAtNsu
+        {
+            get { return string.Format("{0}°", temperature); }
+        }
+
+        private bool IsLoad
+        {
+            set
+            {
+                isLoaded = value;
+                OnPropertyChanged("IsVisibleTextBlock");
+                OnPropertyChanged("IsVisibleBar");
+            }
+        }
+            
+
+        public string IsVisibleTextBlock
+        {
+            get 
+            {
+                if (isLoaded && !isError)
+                    return "Visible";
+                return "Collapsed";
+            }
+        }
+
+        public string IsVisibleBar
+        {
+            get
+            {
+                if (!isLoaded && !isError)
+                    return "Visible";
+                return "Collapsed";
+            }
+        }
+
+        public bool IsError
+        {
+            set
+            {
+                isError = value;
+                OnPropertyChanged("IsVisibleErrorMessage");
+            }
+        }
+
+        public string IsVisibleErrorMessage
+        {
+            get
+            {
+                if (!isError)
+                    return "Collapsed";
+                return "Visible";
+            }
+        }
+
+        public UpdateTemperatureCommand UpdateCommand
+        {
+            get { return updateTemperatureCommand ?? (updateTemperatureCommand = new UpdateTemperatureCommand(UpdateTemperature)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class UpdateTemperatureCommand : ICommand
+    {
+        private Action execute;
+
+        public UpdateTemperatureCommand(Action execute)
+        {
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
