2189945fd403456cca62e00f9be6794af00d34b6
Alexandr Tsvetkov <aleksandr.s.tsvetkov@gmail.com>
2015-10-05 01:15:09 +0200

    Added negative converter

---
+++ WeatherNsuUniversal/WeatherNsuUniversal.Shared/Converters/NegativeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace WeatherNsuUniversal.Converters
+{
+    public class NegativeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return -(double) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
